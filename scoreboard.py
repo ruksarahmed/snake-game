22c5ccf273672e0136a9f426524ffc0ea285d84f
Ruksar Ahmed <ruksar.org@gmail.com>
2022-01-26 18:04:40 +0530

    first commit

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,10 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+<<<<<<< HEAD
+=======
+        self.high_score = 0
+>>>>>>> first commit
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,6 +19,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+<<<<<<< HEAD
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
@@ -26,3 +31,17 @@ class Scoreboard(Turtle):
         self.clear()
         self.update_scoreboard()
 
+=======
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+>>>>>>> first commit
