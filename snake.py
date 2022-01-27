6a10beedf55f27127602fd503ee84eef9544aa36
Ruksar Ahmed <ruksar.org@gmail.com>
2022-01-27 16:16:21 +0530

    Second commit

--- snake.py
+++ snake.py
@@ -26,8 +26,6 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
-<<<<<<< HEAD
-=======
     def reset(self):
         for seg in self.segments:
             seg.goto(1000, 1000)
@@ -35,7 +33,6 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
->>>>>>> first commit
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
